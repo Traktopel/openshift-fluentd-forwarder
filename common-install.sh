ef1ac33ba3b234745be88657bbc5f24d8faf3dec
root <root@cofilopsh06.inail.it>
2018-07-17 11:43:15 +0200

    first try to patch docker images in order to use fluent-plugin-remote_syslog in version >=1.0.0

--- common-install.sh
+++ common-install.sh
@@ -53,7 +53,7 @@ gem install -N --conservative --minimal-deps --no-document \
   fluent-plugin-kubernetes_metadata_filter \
   fluent-plugin-rewrite-tag-filter \
   fluent-plugin-secure-forward \
-  'fluent-plugin-remote_syslog:<1.0.0' \
+  'fluent-plugin-remote_syslog:>=1.0.0' \
   fluent-plugin-splunk-ex
 
 # set up directores so that group 0 can have access like specified in
